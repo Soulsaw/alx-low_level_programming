7f643f849c83b8e05c2932b369fc1fceaa325d2c
Soulsaw <ssawadogo292@gmail.com>
2023-03-18 17:33:40 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 01; a++)
+	for (a = 0; a <= 1; a++)
 	{
 		for (b = 0; b <= 10; b++)
 		{
 		putchar('0' + a);
 		putchar('0' + b);
-		if (a < 01 && b < 10)
+		if (a < 1 && b < 10)
 		{
 		putchar(',');
 		putchar(' ');
