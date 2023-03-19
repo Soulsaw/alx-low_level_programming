e75f3d0dc3c9eb11aa583cbdb6a0e16038603da6
Soulsaw <ssawadogo292@gmail.com>
2023-03-18 18:50:56 -0700

    the last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			if (a < 10)
+			{
+			putchar('0' + a);
+			putchar('0' + a);
+			}
+			else
+			{
+				putchar('0' + a);
+			}
+		putchar(' ');
+			if (b < 10)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+			}
+			else
+			{
+				putchar('0' + b);
+			}
+			if (a == 98 && b == 99)
+				{
+
+				}
+			else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
