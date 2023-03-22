9d11705571a528f07a22f4fdc58a3a321004b7dd
Soulsaw <ssawadogo292@gmail.com>
2023-03-21 21:17:38 -0400

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <unistd.h>
 /**
  * void - the prototype for the print_alphabet function
  * 
@@ -18,5 +19,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+int _putchar(char c);
 #endif
