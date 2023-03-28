1a71773a0f12df190aec3b375f37afebb3e71995
Soulsaw <ssawadogo292@gmail.com>
2023-03-27 21:28:34 -0400

    Pointer, arrays and strings, first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function permit to updates the value it points to 98
+ *
+ * @n: The value of pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
