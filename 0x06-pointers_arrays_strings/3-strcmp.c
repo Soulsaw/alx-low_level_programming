7c96a7b44ca4db3c9e7628fa0bb5e0078bd26603
Soulsaw <ssawadogo292@gmail.com>
2023-03-30 20:57:29 -0400

    Correct awnswer of task 3- strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,26 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int nb1;
-	int nb2;
-	int i;
 
-	i = 0;
-	nb1 = 0;
-	nb2 = 0;
-	while (s1[i] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		nb1 += s1[i];
-		i++;
+		s1++;
+		s2++;
 	}
-
-	
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		nb2 += s2[i];
-		i++;
-	}
-
-	return (nb1 - nb2);
+	return (*s1 - *s2);
 }
