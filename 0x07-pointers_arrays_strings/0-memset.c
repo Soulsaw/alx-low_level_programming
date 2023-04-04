66855f8322015f51568866eef0f1b4d3f79d9f9c
Soulsaw <ssawadogo292@gmail.com>
2023-04-03 20:00:25 -0400

    fix bugg

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+	char *copy = s, value = b;
 
 	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+		copy[i] = value;
 
-	return (s);
+	return (copy);
 }
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _strchr - This function return the first occurence of the character c to s
+ * @s: The destination of pointer
+ * @c: The source of pointer
+ * Return: pointer or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+	int index_occ;
+	int boolean;
+
+	boolean = 0;
+	for (index = 0; index >= '\0'; index++)
+	{
+		if (s[index] == c)
+		{
+			boolean = 1;
+			index_occ = index;
+			break;
+		}
+	}
+	if (boolean == 1)
+	{
+		return (s + index_occ);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
