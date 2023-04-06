b269d9cc1fa740a8571795cbe6bedd998565ea8a
Soulsaw <ssawadogo292@gmail.com>
2023-04-06 13:09:40 -0400

    solve the task 1 and 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This function calcul the pow raised of the integer
+ * @x: The number that we want to calcul the pow
+ * @y: The power of the number
+ * Return: The value of the x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
