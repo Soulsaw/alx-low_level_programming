6a1968f8bf5ea17b3b380e0f8a8fc3396f64301b
Soulsaw <ssawadogo292@gmail.com>
2023-04-13 10:17:43 -0400

    more malloc free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Function that allocates memory
+ * @b: the size of pointer
+ * Return: size or 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (malloc(sizeof(b)) == NULL)
+	{
+		exit(98);
+	}
+	return (void *)(malloc(sizeof(b)));
+}
