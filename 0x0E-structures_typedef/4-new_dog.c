11b15cad315d3b5b5bdfb47eabda5b73756f980f
Soulsaw <ssawadogo292@gmail.com>
2023-04-17 19:37:10 -0400

    fix bugg Structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,27 @@
 #include "dog.h"
+
+/**
+ * _strcpy - copy the string to the dest
+ *
+ * @dest: the destination copy
+ *
+ * @src: the source copy
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
+
 /**
  * new_dog - This function intialize the struct dog
  * @name: The name of dog
@@ -13,10 +36,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = (dog_t *)malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+	dog->name = malloc(sizeof(char *));
+	dog->owner = malloc(sizeof(char *));
 
-	dog->name = name;
+	_strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = owner;
-i
+	_strcpy(dog->owner, owner);
 	return (dog);
 }
