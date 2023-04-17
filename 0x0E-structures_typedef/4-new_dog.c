57cedec9d8d9cdb05afeec210a3ea67a6e0cba14
Soulsaw <ssawadogo292@gmail.com>
2023-04-17 19:22:37 -0400

    fix bugg Structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,4 @@
 #include "dog.h"
-
-char *_strcpy(char *dest, char *src);
-/**
- * _strcpy - copy the string to the dest
- *
- * @dest: the destination copy
- *
- * @src: the source copy
- *
- * Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
-}
-
 /**
  * new_dog - This function intialize the struct dog
  * @name: The name of dog
@@ -38,9 +14,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	_strcpy(dog->name, name);
+	strcpy(dog->name, name);
 	dog->age = age;
-	_strcpy(dog->owner, owner);
+	strcpy(dog->owner, owner);
 i
 	return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * struct dog - The structure to the dog
  * @name: The name of the dog
@@ -20,6 +20,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src)
 
 #endif /* DOG_H */
