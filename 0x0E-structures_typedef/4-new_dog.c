75e0cca27e2335a524db8c5c688170603b8117c3
Soulsaw <ssawadogo292@gmail.com>
2023-04-17 19:47:04 -0400

    fix bugg Structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = (dog_t *)malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char *));
-	dog->owner = malloc(sizeof(char *));
-
+	dog->name = malloc(strlen(name) + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	strcpy(dog->name, name);
-	dog->age = age;
+
+	dog->owner = malloc(strlen(owner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 	strcpy(dog->owner, owner);
+
+	dog->age = age;
+
 	return (dog);
 }
