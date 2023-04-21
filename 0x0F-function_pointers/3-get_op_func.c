f63193e58b65e1c03f886a3600292e7d0c20a13c
Soulsaw <ssawadogo292@gmail.com>
2023-04-21 07:57:59 -0400

    fix bug pointeur function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,18 +9,18 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
+		{"+", &op_add},
+		{"-", &op_sub},
+		{"*", &op_mul},
+		{"/", &op_div},
+		{"%", &op_mod},
 		{NULL, NULL}
 	};
 
 	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0])
+		if (*(ops[i].op) == *s && !*(s + 1))
 			return (ops[i].f);
 		i++;
 	}
