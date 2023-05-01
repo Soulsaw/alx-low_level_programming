cc34aa351ad7a75273ecc255061f7ed95072d911
Soulsaw <87091389+Soulsaw@users.noreply.github.com>
2023-05-01 23:23:25 +0000

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	element->n = n;
 	element->next = NULL;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = element;
 		return (element);
