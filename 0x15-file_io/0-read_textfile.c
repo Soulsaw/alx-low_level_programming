488abd03969fb9c1463766b3e84dafa4fd2ee379
Soulsaw <ssawadogo292@gmail.com>
2023-05-08 20:46:17 -0400

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * read_textfile - This function read the text
+ * @filename: The name of file
+ * @letters: The number of letters it should print
+ * Return: The number of characters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char ch;
+	size_t i = 0;
+
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return (0);
+	do {
+		ch = fgetc(fp);
+		putchar(ch);
+		i++;
+	} while (i < letters && ch != EOF);
+	fclose(fp);
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+# 0x15. C - File I/O
+## 0.Tread lightly, she is near
+Write a function that reads a text and prints it to the POSIX standard output
