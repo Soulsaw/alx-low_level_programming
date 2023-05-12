2dc705e11822c87b569b4db80b9507f105c99ec9
Soulsaw <ssawadogo292@gmail.com>
2023-05-12 01:00:37 +0000

    fix bug error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp < 0)
-		return (-1);
+		return (0);
 	while (i < letters && b != 0)
 	{
 		b = read(fp, &ch, 1);
