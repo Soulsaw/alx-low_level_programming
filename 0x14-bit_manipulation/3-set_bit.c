a38e016beb9b64b7465f0a1e96fd6af3df75363e
Soulsaw <ssawadogo292@gmail.com>
2023-07-06 22:02:54 -0400

    fix error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,5 +8,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	*n = *n | (1 << index);
-	return (if (index >= sizeof(unsigned long int) * 8) ? -1 : 1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	return (1);
 }
