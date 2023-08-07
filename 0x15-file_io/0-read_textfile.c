d7cde792e9dcf5eca25e5588b785bff66f915d49
Soulsaw <ssawadogo292@gmail.com>
2023-08-07 05:20:55 -0400

    Comme in first task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile -  This function print the number of
+ * letters given an arguments
+ * @filename: The name of the file
+ * @letters: The numbers of letters when we want to print
+ * Return: The number of the characters prints
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	int nbr_read;
+	size_t cpt = 0;
+	char c;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while (cpt < letters)
+	{
+		nbr_read = write(STDOUT_FILENO, &c, read(fd, &c, 1));
+		if (nbr_read == 0)
+			return (cpt);
+		if (nbr_read == -1)
+			return (0);
+		cpt++;
+	}
+	close(fd);
+	return (cpt);
+}
