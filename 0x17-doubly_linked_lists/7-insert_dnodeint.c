c3d2235ea33cbb2ac05b34d4c7644570fed06bba
Soulsaw <ssawadogo292@gmail.com>
2023-08-17 13:18:01 -0400

    fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,12 +12,10 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = *h, *tmp, *element = NULL;
-	size_t i = 0, len = dlistint_len(*h);
+	size_t i = 0;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == len - 1)
-		return (add_dnodeint_end(h, n));
 
 	while (current != NULL)
 	{
@@ -25,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		if (idx == i + 1)
 		{
-			add_dnodeint(&element, n);
+			add_dnodeint_end(&element, n);
 			tmp->next = element;
 			element->prev = tmp;
 			element->next = current;
