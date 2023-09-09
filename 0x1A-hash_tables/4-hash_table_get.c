3e27459869736b0975ae9d1153e7ad6096276389
Soulsaw <ssawadogo292@gmail.com>
2023-09-09 13:37:56 -0400

    fixing get task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,13 +11,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *item;
 
 	if (!key || !ht)
 		return (NULL);
 	index = key_index((unsigned const char*)key, ht->size);
-
-	if (ht->array[index])
-		if (strcmp(ht->array[index]->key, key) == 0)
-			return (ht->array[index]->value);
+	item = ht->array[index];
+	if (!item)
+		return (NULL);
+	while (item)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
+	}
+		
 	return (NULL);
 }
