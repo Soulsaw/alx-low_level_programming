fdc208c16c8ef6893b7d8e8082b71cc444c765a4
Soulsaw <ssawadogo292@gmail.com>
2023-09-08 23:01:32 -0400

    fixing task 6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,10 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			strcpy(ht->array[index]->value, value);
 		}
-		else
-		{
-			insert_at_begin(&(ht->array[index]), item);
-		}
 	}
 	return (1);
 }
