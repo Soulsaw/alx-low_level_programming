bfbd74c40879b94511d0add3278592b9a949773b
Soulsaw <ssawadogo292@gmail.com>
2023-12-14 00:41:47 +0000

    end

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+/**
+ * jump_list - This function search a value in an array of list
+ * using the lineary search algorithms
+ * @list: An pointer to the first node of the list
+ * @size: The size of the list
+ * @value: The value to search
+ *
+ * Return: a pointer to the node or NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	if (!list || size == 0)
+		return (NULL);
+	return (NULL);
+}
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+/**
+ * linear_skip - This function search a value in an array of integer
+ * using the lineary search algorithms
+ * @list: An pointer to the first element of the array
+ * @value: The value to search
+ *
+ * Return: node or NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (!list)
+		return (NULL);
+	return (NULL);
+}
