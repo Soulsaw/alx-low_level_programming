862a1e93e87ce96f7703adaac1172c71e165ccd9
Soulsaw <ssawadogo292@gmail.com>
2023-12-30 17:19:29 +0000

    fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char *copy = s, value = b;
 
 	for (i = 0; i < n; i++)
-		copy[i] = value;
+		s[i] = b;
 
-	return (copy);
+	return (s);
 }
