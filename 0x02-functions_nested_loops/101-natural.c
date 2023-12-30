cecd1c3d785cf7f198da9d2e24d27005e12c3c46
Soulsaw <ssawadogo292@gmail.com>
2023-12-30 02:38:22 +0000

    advanced task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - The entry point of the program
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int result = 0, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			result += i;
+	}
+	printf("%d\n", result);
+}
