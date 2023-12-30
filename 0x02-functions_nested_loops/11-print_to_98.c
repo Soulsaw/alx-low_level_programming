f70536a7b1151a83922241f38c8095f9daaa4226
Soulsaw <ssawadogo292@gmail.com>
2023-12-30 02:00:33 +0000

    fix task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,69 @@
 #include "main.h"
 /**
+ * print_number_inf_98 - print the number less than 98
+ * @n: The number to print
+ * Return: nothing
+ */
+void print_number_inf_98(int n)
+{
+	int val;
+
+	if (n < 10)
+	{
+		if (n < 0)
+		{
+			val = n * (-1);
+			_putchar('-');
+			if (val >= 10)
+			{
+				_putchar('0' + val / 10);
+				_putchar('0' + val % 10);
+			}
+			else
+				_putchar('0' + val);
+		}
+		else
+			_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+}
+/**
+ * print_number - print the number less than 98
+ * @n: The number to print
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n >= 100)
+	{
+		_putchar('0' + n / 100);
+		_putchar('0' + n % 100 / 10);
+		_putchar('0' + n  % 100 % 10);
+	}
+	else
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+}
+/**
+ * comma - print the number less than 98
+ * @n: The number to print
+ * Return: nothing
+ */
+void comma(int n)
+{
+	if (n < 98 || n > 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+/**
  * print_to_98 - print the numbers between n and 98
  * @n: The number to compare which 98
  */
@@ -9,29 +73,21 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			putchar('1' + n);
-			if (n < 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			print_number_inf_98(n);
+			comma(n);
 			n++;
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		while (n >= 98)
 		{
-			putchar('1' + n);
-			if (n > 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			print_number(n);
+			comma(n);
 			n--;
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 
 }
