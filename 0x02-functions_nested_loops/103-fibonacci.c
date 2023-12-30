56b55b1d0ed31bce9d62b0d15407e402d9f4e61d
Soulsaw <ssawadogo292@gmail.com>
2023-12-30 09:19:38 +0000

    fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,17 @@
 int main(void)
 {
 	long int cur = 1, next = 2, fb;
-	int cpt = 2;
+	int even_valued = 2;
 
 	while (fb <= 4000000)
 	{
 		fb = cur + next;
 		cur = next;
 		next = fb;
-		cpt++;
+		if (fb % 2 == 0)
+			even_valued += fb;
 	}
-	printf("%d\n", cpt);
+	printf("%d\n", even_valued);
 
 	return (0);
 }
