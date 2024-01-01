578e1aec7ec3e2c47013866a2cdd791bff7c020e
Soulsaw <ssawadogo292@gmail.com>
2024-01-01 00:56:27 +0000

    fix bug

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,21 +10,14 @@ int check_sqrt_recursion(int n, int b)
 {
 
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else
+	if (n != 0)
 	{
-		if (n != 0)
-		{
-			if (n < 0)
-				return (-1);
-			n = n - (2 * b + 1);
-			b++;
-			return (check_sqrt_recursion(n, b));
-		}
-		return (b);
+		n = n - (2 * b + 1);
+		b++;
+		return (check_sqrt_recursion(n, b));
 	}
+	return (b);
 }
 
 /**
